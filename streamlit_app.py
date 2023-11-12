393c0e009b9834d341196a4e7546419fd0dd0305
Pablo <example@gmail.com>
2023-11-12 03:13:21 +0100

    Improves streamlit frontend. Hopeully the final iteration.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,21 +2,27 @@ import streamlit as st
 from streamlit_image_select import image_select
 import pandas as pd
 from PIL import Image
+import subprocess
 
 """
 # Outfit generator demo
 Select an image you want to generate a new outfit from.
 """
 
-if 'selection' not in st.session_state:
-    nprdf = pd.read_csv('processed_data/clean_product_data_with_filenames.csv')
-    nprdf = nprdf[['cod_modelo_color', 'des_filename']]
-    nprdf = nprdf.sample(n=10)
+
+def reroll():
+    nrawdf = pd.read_csv('processed_data/clean_product_data_with_filenames.csv')
+    nrawdf = nrawdf[['cod_modelo_color', 'des_filename']]
+    nprdf = nrawdf.sample(n=10)
+    st.session_state['rawdf'] = nrawdf
     st.session_state['selection'] = nprdf
 
-prdf = st.session_state['selection']
 
-#prdf
+if 'selection' not in st.session_state:
+    reroll()
+
+rawdf = st.session_state['rawdf']
+prdf = st.session_state['selection']
 
 sqrimgs = []
 
@@ -34,5 +40,22 @@ chosen_idx = image_select(
     use_container_width=False
 )
 
+if st.button('Get different products'):
+    reroll()
+    st.rerun()
 if st.button('Generate fresh outfit'):
-    chosen_idx
+    fileq = open("request.txt", 'w')
+    fileq.write(prdf.iloc[chosen_idx]['cod_modelo_color'])
+    fileq.close()
+    #subprocess.run(["gen"])
+    filea = open("answer.txt", 'r')
+    lines = filea.readlines()
+    filea.close()
+    imgs = []
+    for line in lines:
+        line = line.rstrip()
+        my_row = rawdf.loc[rawdf["cod_modelo_color"] == line]
+        my_img = Image.open(my_row.iloc[0]['des_filename'])
+        imgs.append(my_img)
+    st.image(imgs, width=225)
+
